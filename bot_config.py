fb5937366193a7d3e0810e4522fa289f41bf8c0b
Feduk27 <103634666+Feduk27@users.noreply.github.com>
2023-02-07 15:51:54 +0300

    Update bot_config.py

--- bot_config.py
+++ bot_config.py
@@ -1,2 +1,3 @@
+#token hidden for security reasons
 https = 'https://api.telegram.org/bot5803413207:AAFhnMO80eHVg9BcFJXAMy4DtLm1j86_dz4/getMe'
-token = '5803413207:AAFhnMO80eHVg9BcFJXAMy4DtLm1j86_dz4'
+token = 'token'
